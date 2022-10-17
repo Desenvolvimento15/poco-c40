abe47dee8fc53d3efe659e752ed8027e902a4415
Desenvolvimento15 <96912707+Desenvolvimento15@users.noreply.github.com>
2022-10-17 11:13:39 -0300

    vs01

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".galeria").slick({
+        arrows: false,
+        dots: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});    
